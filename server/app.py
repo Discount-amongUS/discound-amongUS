f4ce1071ac5b09b88b323b887807f1a609eedca4
Edgar <epineda0313@gmail.com>
2020-12-03 13:11:32 -0500

    Added more setup

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 
 from config import config
-from model import *
+from models import *
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -9,6 +10,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = config.URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
+from routes import *
 
 if __name__ == '__main__':
     app.run()
