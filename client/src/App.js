b678028b1d1064206cef2e914fa079ec26a804a4
Edgar <epineda0313@gmail.com>
2020-12-11 15:21:07 -0500

    Added auth context to frontend able to get id from jwt

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component,  } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import { AuthProvider } from "./context/authContext";
 import Home from "./components/pages/Home/Home";
+import PrivateRoute from "./privateRoute";
 
 export class App extends Component {
   render() {
     return (
-
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </Router>
-
+      <AuthProvider>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </Router>
+      </AuthProvider>
     );
   }
 }
---
+++ client/src/privateRoute.js
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import { Redirect, Route } from "react-router-dom";
+import { AuthContext } from "./context/authContext";
+
+function PrivateRoute({ component: Component, ...rest }) {
+  const { checkTokenExist }  = useContext(AuthContext);
+  // In the future when we need more properties from the React Context , we can just use
+  // Javascript destructing to get it. 
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        checkTokenExist() ? (
+          <Component {...props} />
+        ) : (
+            <Redirect to="/login" />
+        )
+      }
+    />
+  );
+}
+
+export default PrivateRoute;
